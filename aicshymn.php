15b964fa7fabab8d7ccd0a1648cc3891556b89b5
mharklent <46800764+mharklent@users.noreply.github.com>
2019-11-26 15:02:48 +0800

    Add files via upload

---
+++ aicshymn.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $break = "</br>";
+        echo "<h2>AICS Hymn</h2>";
+        echo $break;
+        echo "<b><p>AICS HYMN</p><b>";
+        echo "<p>In a time of dark despair</p>";
+        echo "<p>You are like a ray of hope</p>";
+        echo "<p>In a world that's full of hunger</p>";
+        echo "<p>You are there to feed our souls..</p>";
+        echo "<p>And make us strong to lift ourselves,</p>";
+        echo "<p>To be the best of what we are.!</p>";
+        echo $break;
+        echo "<p>Like a beacon on the shore,</p>";
+        echo "<p>You are there to guide our way</p>";
+        echo "<p>Like a wind that's ever blowing,</p>";
+        echo "<p>You are there to lift up .</p>";
+        echo "<p>And give us wings so we can fly,</p>";
+        echo "<p>And claim the star that lies beyond.!</p>";
+    ?>
+</body>
+</html>
